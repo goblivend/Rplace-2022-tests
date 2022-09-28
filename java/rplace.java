d24fa53d5b16a49c2667ee51f6d96ba352a7c970
Ivan Imbert <ivan.imbert@epita.fr>
2022-09-28 19:28:53 +0200

    Update rplace.java

--- java/rplace.java
+++ java/rplace.java
@@ -12,14 +12,12 @@ import java.awt.*;
 
 public class rplace {
     public static void main(String[] args) {
-        // int sizeSample = 100000000;
         try {
-            // String fileName = "../sample " + sizeSample + ".csv";
             String fileName = args[0]; //"../Day 5.csv";
             long tStart = System.currentTimeMillis();
-            BufferedImage img = getIMG(fileName);
+            // getIMG(fileName);
+            firstPixel(fileName);
             long tEnd = System.currentTimeMillis();
-
 
             System.out.println(tEnd - tStart);
 
@@ -29,7 +27,7 @@ public class rplace {
 
     }
 
-    public static BufferedImage getIMG(String fileName) {
+    public static void getIMG(String fileName) {
         try {
             BufferedImage rplace = new BufferedImage(2000, 2000, BufferedImage.TYPE_INT_ARGB);
             File f = new File(fileName);
@@ -37,16 +35,9 @@ public class rplace {
             reader.nextLine();
             int line = 0;
 
-
-
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
-                // System.out.println(data);
-
-                String ts = data.split(",")[0];
-                // System.out.println(ts);
-
-
+                // String ts = data.split(",")[0];
 
                 String hexColor = data.substring(data.indexOf("#") + 1, data.indexOf("#") + 7);
                 int color = Integer.parseInt(hexColor, 16);
@@ -70,7 +61,52 @@ public class rplace {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return null;
+    }
+
+    public static void firstPixel(String fileName) {
+        try {
+            BufferedImage rplace = new BufferedImage(2000, 2000, BufferedImage.TYPE_INT_ARGB);
+            File f = new File(fileName);
+            Scanner reader = new Scanner(f);
+            reader.nextLine();
+            int line = 0;
+            int nbPixel = 2000*2000;
+            int pixel = 0;
+
+
+            while (reader.hasNextLine() && pixel < nbPixel) {
+                String data = reader.nextLine();
+
+                String hexColor = data.substring(data.indexOf("#") + 1, data.indexOf("#") + 7);
+                int color = Integer.parseInt(hexColor, 16);
+
+                String coordinates = data.substring(data.indexOf("\"") + 1, data.length() - 1);
+                if (coordinates.length() > 9) {
+                    System.out.println(data);
+                    continue;
+                }
+                String xStr = coordinates.substring(0, coordinates.indexOf(","));
+                String yStr = coordinates.substring(coordinates.indexOf(",") + 1, coordinates.length());
+                int x = Integer.parseInt(xStr);
+                int y = Integer.parseInt(yStr);
+
+                if (rplace.getRGB(x, y) == 0){
+                    rplace.setRGB(x, y, new Color(color).getRGB());
+                    pixel += 1;
+                }
+
+
+
+                line += 1;
+            }
+            reader.close();
+            System.out.println("Out of " + nbPixel + " new pixels, " + pixel + " where placed and " + (nbPixel-pixel) + " are still missing");
+            File outputfile = new File(fileName + " first pixels.png");
+            ImageIO.write(rplace, "png", outputfile);
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
 // "YYYY-MM-DD HH-mm-ss.SSS zzz"
